d58ee5dcbd406c59f9e25f5402a66c461f723a9e
Benedikt Burger <67148916+BenediktBurger@users.noreply.github.com>
2026-06-17 09:08:06 +0200

    Fix cast for instruments

--- pymeasure/instruments/instrument.py
+++ pymeasure/instruments/instrument.py
@@ -28,7 +28,7 @@ from typing import TypeVar
 from collections.abc import Sequence
 from warnings import warn
 
-from .common_base import CommonBase
+from .common_base import CommonBase, cast_or_str
 from ..adapters.adapter import Adapter
 from ..adapters.visa import VISAAdapter
 
@@ -100,6 +100,52 @@ class Instrument(CommonBase):
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool | None:
         self.shutdown()
         return None
+
+    # SCPI default properties
+    @property
+    def complete(self):
+        """Get the synchronization bit.
+
+        This property allows synchronization between a controller and a device. The Operation
+        Complete query places an ASCII character 1 into the device's Output Queue when all pending
+        selected device operations have been finished.
+        """
+        if self.SCPI:
+            return self.ask("*OPC?").strip()
+        else:
+            raise NotImplementedError("Non SCPI instruments require implementation in subclasses")
+
+    @property
+    def status(self):
+        """ Get the status byte and Master Summary Status bit. """
+        if self.SCPI:
+            return self.ask("*STB?").strip()
+        else:
+            raise NotImplementedError("Non SCPI instruments require implementation in subclasses")
+
+    @property
+    def options(self):
+        """ Get the device options installed. """
+        if self.SCPI:
+            return self.ask("*OPT?").strip()
+        else:
+            raise NotImplementedError("Non SCPI instruments require implementation in subclasses")
+
+    @property
+    def id(self):
+        """ Get the identification of the instrument. """
+        if self.SCPI:
+            return self.ask("*IDN?").strip()
+        else:
+            raise NotImplementedError("Non SCPI instruments require implementation in subclasses")
+
+    @property
+    def next_error(self):
+        """Get the next error of the instrument (tuple of code and message)."""
+        if self.SCPI:
+            return self.values("SYST:ERR?", cast=cast_or_str(float))
+        else:
+            raise NotImplementedError("Non SCPI instruments require implementation in subclasses")
 
     # Wrapper functions for the Adapter object
     def write(self, command: str, **kwargs) -> None:
